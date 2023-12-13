da86c7416df3b11be810b187e85648973c3fb786
SwiftDev12 <134415973+SwiftDev12@users.noreply.github.com>
2023-12-13 06:22:06 +0000

    Add files via upload

---
+++ repo_system_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  repo_system_testApp.swift
+//  repo system test
+//
+//  Created by Matty on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct repo_system_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
